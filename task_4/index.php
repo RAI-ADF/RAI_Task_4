d664ea6501eee4ab27db8f51f53366cd86ef1b00
aridakartika <aridakartika@outlook.com>
2015-09-16 05:25:38 -1200

    creating index

--- task_4/index.php
+++ task_4/index.php
@@ -1,1 +1,11 @@
-
+<?php
+	$dbhost = 'localhost';
+	$dbuser = 'root';
+	$dbpass = '';
+	$connection = mysql_connect($dbhost, $dbuser, $dbpass);
+	if(! $connection ){
+	  die('Could not connect: ' . mysql_error());
+	}
+	echo 'Connected successfully';
+	mysql_close($connection);
+?>
