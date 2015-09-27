431a2c5401678803a6244ae6fb6ba39f6e6261a0
nendijuned <nendi.juned15@gmail.com>
2015-09-28 06:12:00 +0700

    change file login.php

--- task_4/login.php
+++ task_4/login.php
@@ -1,1 +1,20 @@
+<html>
+<head>
+<title>Log In</title>
+<link rel="stylesheet" type="text/css" href="../css/login-css.css">
+</head>
 
+<body>
+	<form id="login">
+        <h1>Log In</h1>
+        <fieldset id="inputs">
+            <input id="username" type="text" placeholder="Username" autofocus="" required="">   
+            <input id="password" type="password" placeholder="Password" required="">
+        </fieldset>
+        <fieldset id="actions">
+            <input type="submit" id="submit" value="Log in">
+            <a href="">Forgot your password?</a><a href="registration.php">Register</a>
+        </fieldset>
+    </form>
+</body>
+</html>
