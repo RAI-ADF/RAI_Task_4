38b29fe09d34989bdc52b9e662281ee942149518
aji tri santoso <ajitrisantoso@gmail.com>
2015-09-27 13:16:06 +0700

    adding exported .sql

--- task_4/login.php
+++ task_4/login.php
@@ -14,37 +14,37 @@ mysql_select_db($mysql_db_database, $con) or die("Could not select database");
 // $password ="1234";
 if(isset($_POST["username"]) && isset($_POST["password"])) {
 
-$username = mysql_real_escape_string( $_POST["username"] );
-$password = mysql_real_escape_string( $_POST["password"] );
+	$username = mysql_real_escape_string( $_POST["username"] );
+	$password = mysql_real_escape_string( $_POST["password"] );
 
 
 // echo $username;
 // echo $password;
 
-$query = "SELECT * FROM user WHERE username='$username' and password='$password'";
+	$query = "SELECT * FROM user WHERE username='$username' and password='$password'";
 
-$result = mysql_query($query)or die(mysql_error());
+	$result = mysql_query($query)or die(mysql_error());
 
-$num_row = mysql_num_rows($result);
-		$row=mysql_fetch_array($result);
-		if( $num_row >=1 ) {
+	$num_row = mysql_num_rows($result);
+	$row=mysql_fetch_array($result);
+	if( $num_row >=1 ) {
 
-			echo 'true';
-			$_SESSION['username']=$row['username'];		
-			$_SESSION['user_type'] =$row['user_type'];
-			if ($_SESSION['user_type'] ==1) {
-				echo "admin";
-				header("Location: adminPage.php");
-				
-			}elseif ($_SESSION['user_type'] ==2) {
-				echo "user";
-				 header("Location: clientPage.php");
-			}
-				
+		echo 'true';
+		$_SESSION['username']=$row['username'];		
+		$_SESSION['user_type'] =$row['user_type'];
+		if ($_SESSION['user_type'] ==1) {
+			echo "admin";
+			header("Location: adminPage.php");
+			
+		}elseif ($_SESSION['user_type'] ==2) {
+			echo "user";
+			header("Location: clientPage.php");
 		}
-		else{
-			echo 'User not found  <a href=registration.php>Click Here to Register</a> ';
-		}
+		
+	}
+	else{
+		echo 'User not found  <a href=registration.php>Click Here to Register</a> ';
+	}
 }
 
 ?>
