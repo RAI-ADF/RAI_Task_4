04d3337e942d4d1e9f98d59c7a68eed9530d46fc
TaufikAk <akbartaufik9@gmail.com>
2015-09-28 23:26:16 +0700

    1103120020

---
+++ indeks.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Indeks</title>
+</head>
+
+<body>
+<?php include_once "menu.php"; ?>
+<br><br>
+<p>Apakah anda sudah memiliki akun?
+	Apabila sudah klik <a href="Login.php">LOGIN</a></p>
+<p>Apabila belum, silahkan klik <a href="Registrasi.php">REGISTRASI</a></p>
+</body>
+</html>
