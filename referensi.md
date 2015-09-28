3bf3cfbb43936c268d7fc9c18c7d6c99f14e63eb
ajisantoso <ajitrisantoso@gmail.com>
2015-09-28 23:34:07 +0700

    adding adminpage,clientpage

--- referensi.md
+++ referensi.md
@@ -2,3 +2,4 @@ Referensi:
 <br>http://stackoverflow.com/questions/14679055/php-sql-server-login-sessions
 <br>http://stackoverflow.com/questions/12382972/redirect-to-admin-user-page-after-login
 <br>http://www.w3schools.com/php/php_mysql_insert.asp
+<br>http://www.phpform.org/formbuilder/index.php
--- task_4/adminPage.php
+++ task_4/adminPage.php
@@ -1,41 +1,68 @@
-<!doctype html>
-
-<html lang="en">
-
+<!DOCTYPE html>
+<html>
 <head>
-
-  <meta charset="utf-8">
-
-  <title>jQuery UI Datepicker - Default functionality</title>
-
-  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-
-  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-
-
-  <script>
-
-  $(function() {
-
-    $( "#datepicker" ).datepicker();
-
-  });
-
-  </script>
-
+	<title>Admin Page</title>
 </head>
-
 <body>
+	<div align="center" >
+		<?php
+		$username="root";
+		$password="";
+		$database="rai_task4";
 
- 
+		mysql_connect("localhost",$username,$password);
+		@mysql_select_db($database) or die( "Unable to select database");
 
-<p>Date: <input type="text" id="datepicker"></p>
+		$query="SELECT * FROM user";
+		$result=mysql_query($query);
+		$num=mysql_numrows($result);
+		mysql_close();
+		?>
 
- 
+		<table border="0" cellspacing="2" cellpadding="2">
+			<tr>
+				<td>
+					<font face="Arial, Helvetica, sans-serif">Name</font>
+				</td>
+				<td>
+					<font face="Arial, Helvetica, sans-serif">Username</font>
+				</td>
+				<td>
+					<font face="Arial, Helvetica, sans-serif">email</font>
+				</td>
+				<td>
+					<font face="Arial, Helvetica, sans-serif">password</font>
+				</td>
+				<td>
+					<font face="Arial, Helvetica, sans-serif">Tanggal Lahir</font>
+				</td>
+			</tr>
+			<?php
+			$i=0;
+			while ($i < $num) {$f1=mysql_result($result,$i,"name");
+			$f2=mysql_result($result,$i,"username");$f3=mysql_result($result,$i,"email");
+			$f4=mysql_result($result,$i,"password");$f5=mysql_result($result,$i,"date_of_birth");?>
+			<tr>
+				<td>
+					<font face="Arial, Helvetica, sans-serif"><?php echo $f1; ?></font>
+				</td>
+				<td>
+					<font face="Arial, Helvetica, sans-serif"><?php echo $f2; ?></font>
+				</td>
+				<td>
+					<font face="Arial, Helvetica, sans-serif"><?php echo $f3; ?></font>
+				</td>
+				<td>
+					<font face="Arial, Helvetica, sans-serif"><?php echo $f4; ?></font>
+				</td>
+				<td>
+					<font face="Arial, Helvetica, sans-serif"><?php echo $f5; ?></font>
+				</td>
+			</tr>
+			<?php
+			$i++;
+		}?>
 
- 
-
+	</div>
 </body>
-
 </html>
-
