cb124c7f446e0b73024868a5f5c9f5ed82c30b3c
Ryan Sukma Nesya <ryanesya5@gmail.com>
2015-09-28 23:30:16 +0700

    Update login.php

--- task_4/login.php
+++ task_4/login.php
@@ -1,1 +1,23 @@
+<?php
+include "config.php";
+$username = $_POST['username'];
+$password = $_POST['password'];
+if (empty($username)){
+	echo "<script>alert('Username belum diisi')</script>";
+	include "login.php";
+}else if (empty($password)){
+	echo "<script>alert('Password belum diisi')</script>";
+	include "login.php";
+}else{
+	session_start();
+	$login = mysql_query("select * from users where username='$username' and password='$password'");
+	if (mysql_num_rows($login) > 0){
+		$_SESSION['username'] = $username;
+		header("location:index.html");
+	}else{
+		echo "<script>alert('Username atau Password salah')</script>";
+		include "login.php";
+	}
+}
+?>
 
