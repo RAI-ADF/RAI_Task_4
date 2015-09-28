8a9e163de8e082963ec069efe876aa0b661ecb0f
erpinalghifari <gmerpin@yahoo.com>
2015-09-28 21:59:04 +0700

    nambah dropdown

--- task_4/koneksi.php
+++ task_4/koneksi.php
@@ -1,3 +1,7 @@
-<?php $host = "localhost"; $user = "root"; $pass = ""; $db = "dbrai"; 
+<?php 
+$host = "localhost"; 
+$user = "root"; 
+$pass = ""; 
+$db = "dbrai"; 
 $konek = mysql_connect($host, $user, $pass) or die ('Koneksi Gagal! '); 
 mysql_select_db($db); ?>
