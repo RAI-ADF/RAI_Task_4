148eba9958ec9d698e17fe25c8874bd980aa4bef
nonieck <nonieck@gmail.com>
2015-09-28 23:13:51 +0700

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,17 @@
+
+<div id='cssmenu'>
+<ul>
+   <li><a href='home.php'>Home</a></li>
+   <li class='active has-sub'><a href='#'>Activity</a>
+      <ul>
+         <li><a href='login.php'>Login</a>
+            
+         </li>
+         <li><a href='regist.php'>Register</a>
+            
+         </li>
+      </ul>
+   </li>
+   <li><a href='aboutme.php'>About</a></li>
+</ul>
+</div>
