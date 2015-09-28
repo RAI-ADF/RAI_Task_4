e18bb1461486f385627c4a9234a49ad4ad33d5d3
Arvin Chendriyana Supriyadi <arvinchs@gmail.com>
2015-09-28 21:40:13 +0700

    Create connect.php

---
+++ 1103120216/connect.php
@@ -0,0 +1,8 @@
+<?php
+define('dbserver', 'localhost'); // Your Database host
+define('dbusername', 'root'); // Your Database user name
+define('dbpassword', ''); // Your Database password
+define('dbdatabase', 'registrasi'); // Your Database Name
+
+$conn= mysqli_connect(dbserver,dbusername,dbpassword,dbdatabase); // Connection
+?>
